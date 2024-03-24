b66a188faec1d8f83baf96a27bd6cd44dfd63b84
Magali   Leodato <103657776+Magalileodato@users.noreply.github.com>
2024-03-24 11:58:51 -0300

    Update cadastro_participantes.dart

--- lib/cadastro_participantes.dart
+++ lib/cadastro_participantes.dart
@@ -71,3 +71,78 @@ class _CadastroParticipantesStateState extends State<_CadastroParticipantesState
 }
 
 //Açoes referente a págiana 
+
+// TelaCastro é um StatefulWidget responsável pela tela de cadastro.
+class TelaCastro extends StatefulWidget {
+  const TelaCastro({Key? key}) : super(key: key);
+
+  @override
+  _TelaCastroState createState() => _TelaCastroState();
+}
+
+// _TelaCastroState é a classe que mantém o estado da tela de cadastro.
+class _TelaCastroState extends State<TelaCastro> {
+  final TextEditingController _emailController = TextEditingController();
+  final TextEditingController _passwordController = TextEditingController();
+
+  @override
+  void dispose() {
+    _emailController.dispose();
+    _passwordController.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Cadastro'),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            // Campo de entrada para o email.
+            TextField(
+              controller: _emailController,
+              decoration: const InputDecoration(
+                labelText: 'Email',
+              ),
+            ),
+            const SizedBox(height: 16.0),
+            // Campo de entrada para a senha.
+            TextField(
+              controller: _passwordController,
+              obscureText: true, // Mascara o texto digitado
+              decoration: const InputDecoration(
+                labelText: 'Senha',
+              ),
+            ),
+            const SizedBox(height: 32.0),
+             // Botão para submeter o formulário de cadastro.
+            ElevatedButton(
+              onPressed: () {
+                // Obtém o email e a senha digitados
+                String email = _emailController.text;
+                String password = _passwordController.text;
+                // Registra o novo email e senha utilizando o logger.
+                logeer.d('Novo Email: $email');
+                logeer.d('Nova Senha: $password');
+              },
+              child: const Text('Cadastrar'),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+  
+}
+// Não é necessário definir o mixin logeer neste caso, 
+// porque estamos usando o logger diretamente no _TelaCastroState.
+mixin logeer {
+  static void d(String s) {}
+}
+
+
