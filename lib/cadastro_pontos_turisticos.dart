19a1050d077b0dfc6b28797ad6d50d002132e73f
Magali   Leodato <103657776+Magalileodato@users.noreply.github.com>
2024-04-23 15:32:29 -0300

    Update cadastro_pontos_turisticos.dart

--- lib/cadastro_pontos_turisticos.dart
+++ lib/cadastro_pontos_turisticos.dart
@@ -27,7 +27,7 @@ class _CadastroPontosTuristicosState extends StatefulWidget {
 }
 
 class _CadastroPontosTuristicosStateState extends State<_CadastroPontosTuristicosState> {
-  // Criação do slogn da página
+ // Criação do slogn da página
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -38,40 +38,49 @@ class _CadastroPontosTuristicosStateState extends State<_CadastroPontosTuristico
         child: ListView(
           children: <Widget>[
             Container(
-              margin: const EdgeInsets.only(top: 20), // Espaço supeior da imagem
-              child: ClipRRect(
-                borderRadius: BorderRadius.circular(40),
-                child: Image.asset(
-                  'images/livro.jpg',
-                  width: 50, //Tamanho da imagem
-                  height: 50,
-                ),
+              color: Colors.blue, // Define a cor de fundo como azul
+              padding: const EdgeInsets.symmetric(vertical: 10), // Adiciona um espaçamento interno
+              child: Column(
+                children: <Widget>[
+                  Container(
+                    margin: const EdgeInsets.only(top: 20), // Adiciona espaço acima da imagem
+                    child: ClipRRect(
+                      borderRadius: BorderRadius.circular(40),
+                      child: Image.asset(
+                        'images/livro.jpg',
+                        width: 70,
+                        height: 70,
+                      ),
+                    ),
+                  ),
+                  const SizedBox(height: 10),
+                  const Text(
+                    'Universidade sem Fronteiras',
+                    style: TextStyle(
+                      fontSize: 18,
+                      fontWeight: FontWeight.bold, //Deixa o texto em negrito
+                      color: Colors.white, // Adiciona a cor branca ao texto
+                    ),
+                    textAlign: TextAlign.center, // deixando o texto centralizado
+                  ),
+                  const SizedBox(height: 15), // Espaço em branco antes da linha horizontal
+                  const Divider(
+                    height: 10,
+                    color: Colors.white, // Ajuste da  cor e criação de uma linha orizontal 
+                    thickness: 4, // Ajuste a espessura da linha 
+                    indent: 12, // Espaçamento à esquerda
+                    endIndent: 10, // Espaçamento à direita
+                  ),
+                ],
               ),
             ),
-            const SizedBox(height: 5),
-            const Text(
-              'Universidade sem Fronteiras',
-              style: TextStyle(
-                fontSize: 15, // Tamanho da Fonte
-                fontWeight: FontWeight.bold, //Estilo da fonte negrito
-                color: Colors.black,
-              ),
-              textAlign: TextAlign.center, // Aliando o texto no centro
-            ),
-            const SizedBox(height: 10),
-            const Divider(
-              height: 2,
-              color: Colors.black,
-              thickness: 4, // Espesura da linha
-              indent: 10,
-              endIndent: 10,
-            ),
-          ], 
+          ],
         ),
       ),
     );
   }
 }
+
 
 //Açoes referente a págiana 
 
